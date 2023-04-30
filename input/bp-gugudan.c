91b045ddd57a1cab478103a0b8be09940bd047d0
Kijun Shin <sharaelong.shin@gmail.com>
2023-05-01 00:43:10 +0900

    Add initial BranchPredictPass

---
+++ input/bp-gugudan.c
@@ -0,0 +1,13 @@
+#include <inttypes.h>
+
+uint64_t read(void);
+void write(uint64_t val);
+
+int main() {
+    for (int i=2; i<=9; ++i) {
+        for (int j=2; j<=9; ++j) {
+            int k = i * j;
+        }
+    }
+    return 0;
+}
