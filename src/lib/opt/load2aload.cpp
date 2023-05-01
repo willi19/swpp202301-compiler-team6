cf45a50933b32cd112b26dd9a2cc93818e4089ba
Sungbin Jo <goranmoomin@daum.net>
2023-05-01 14:56:33 +0900

    Add threshold on how much aloads get pushed

--- src/lib/opt/load2aload.cpp
+++ src/lib/opt/load2aload.cpp
@@ -1,14 +1,115 @@
 #include "load2aload.h"
 #include "llvm/IR/Function.h"
-#include "llvm/IR/IRBuilder.h"
 #include "llvm/IR/InstrTypes.h"
+#include "llvm/IR/Instructions.h"
 #include "llvm/IR/IntrinsicInst.h"
 #include "llvm/IR/PassManager.h"
+
 #include <queue>
 
 using namespace llvm;
 
 namespace {
+bool isAloadSWPPIntrinsic(Instruction *I) {
+  if (auto *CI = dyn_cast<CallInst>(I)) {
+    auto N = CI->getName();
+    return N == "aload_i8" || N == "aload_i16" || N == "aload_i32" ||
+           N == "aload_i64";
+  }
+  return false;
+}
+
+bool isSumSWPPIntrinsic(Instruction *I) {
+  if (auto *CI = dyn_cast<CallInst>(I)) {
+    auto N = CI->getName();
+    return N == "int_sum_i1" || N == "int_sum_i8" || N == "int_sum_i16" ||
+           N == "int_sum_i32" || N == "int_sum_i64";
+  }
+  return false;
+}
+
+bool isIncrDecrSWPPIntrinsic(Instruction *I) {
+  if (auto *CI = dyn_cast<CallInst>(I)) {
+    auto N = CI->getName();
+    return N == "incr_i1" || N == "incr_i8" || N == "incr_i16" ||
+           N == "incr_i32" || N == "incr_i64" || N == "decr_i1" ||
+           N == "decr_i8" || N == "decr_i16" || N == "decr_i32" ||
+           N == "decr_i64";
+  }
+  return false;
+}
+
+bool isAssertSWPPIntrinsic(Instruction *I) {
+  if (auto *CI = dyn_cast<CallInst>(I)) {
+    auto N = CI->getName();
+    return N == "assert_eq_i1" || N == "assert_eq_i8" || N == "assert_eq_i16" ||
+           N == "assert_eq_i32" || N == "assert_eq_i64";
+  }
+  return false;
+}
+
+bool isSWPPIntrinsic(Instruction *I) {
+  return isAloadSWPPIntrinsic(I) || isIncrDecrSWPPIntrinsic(I) ||
+         isAssertSWPPIntrinsic(I);
+}
+
+int getExpectedCost(Instruction *I) {
+  int Cost = 0;
+  if (isa<ReturnInst>(I) || isa<BranchInst>(I)) {
+    // Note: terminator instructions don't need cost calculation in this case
+    Cost = 0;
+  } else if (isa<LoadInst>(I)) {
+    // TODO: Check how to determine if pointer is stack or heap
+    Cost = (20 + 30) / 2;
+  } else if (isa<StoreInst>(I)) {
+    Cost = (20 + 30) / 2;
+  } else if (auto *CI = dyn_cast<CallInst>(I)) {
+    if (isAloadSWPPIntrinsic(CI)) {
+      Cost = 1;
+    } else if (isSumSWPPIntrinsic(CI)) {
+      Cost = 10;
+    } else if (isIncrDecrSWPPIntrinsic(CI)) {
+      Cost = 1;
+    } else if (isAssertSWPPIntrinsic(CI)) {
+      Cost = 0;
+    } else {
+      // Check if it is always function call
+      Cost = 2 + CI->getNumOperands();
+    }
+  } else if (isa<ICmpInst>(I)) {
+    Cost = 1;
+  } else if (isa<SelectInst>(I)) {
+    Cost = 1;
+  } else {
+    switch (I->getOpcode()) {
+    case Instruction::UDiv:
+    case Instruction::SDiv:
+    case Instruction::URem:
+    case Instruction::SRem:
+    case Instruction::Mul:
+      Cost = 1;
+      break;
+    case Instruction::Shl:
+    case Instruction::LShr:
+    case Instruction::AShr:
+    case Instruction::And:
+    case Instruction::Or:
+    case Instruction::Xor:
+      Cost = 4;
+      break;
+    case Instruction::Add:
+    case Instruction::Sub:
+      Cost = 5;
+      break;
+    default:
+      // errs() << "Unhandled Opcode " << I->getOpcodeName() << "\n";
+      // llvm_unreachable("No opcodes should be left unhandled");
+      Cost = 0;
+      break;
+    }
+  }
+  return Cost;
+}
 bool canStoreMemory(Instruction *I) {
   if (isa<StoreInst>(I)) {
     return true;
@@ -30,8 +131,14 @@ PreservedAnalyses Load2AloadPass::run(Function &F,
   for (auto &BB : F) {
     MapVector<LoadInst *, Instruction *> ReplaceMap;
     std::deque<Instruction *> InstQueue;
+    int InstCost = 0;
 
     for (auto &I : BB) {
+      while (!InstQueue.empty() &&
+             InstCost - getExpectedCost(InstQueue.front()) > 34) {
+        InstCost -= getExpectedCost(InstQueue.front());
+        InstQueue.pop_front();
+      }
       if (auto *LI = dyn_cast<LoadInst>(&I)) {
         if (!InstQueue.empty()) {
           ReplaceMap[LI] = InstQueue.front();
@@ -40,8 +147,10 @@ PreservedAnalyses Load2AloadPass::run(Function &F,
 
       if (canStoreMemory(&I)) {
         InstQueue.clear();
+        InstCost = 0;
       }
       InstQueue.push_back(&I);
+      InstCost += getExpectedCost(&I);
     }
 
     for (auto &[LI, InsertPos] : ReplaceMap) {
